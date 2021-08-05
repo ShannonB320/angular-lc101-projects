7317c3c55aef11fbc1b20b6bf4aec8031f6b8044
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-08-05 14:55:22 -0500

    Exercises 28.7.1 and 28.7.2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo Gallery';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://petcostumecenter.com/wp-content/uploads/2020/05/Screen-Shot-2018-09-20-at-8.50.29-PM.png';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvAiVh6yJqMf5sUXQIzAhA1eB21ylTDFKZytloDt8F4Ii-_NXsAO3BIXD3NShID_BSV5s&usqp=CAU';
 
   constructor() { }
 
